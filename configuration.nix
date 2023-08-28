68ff9db48ef27919256c52847d335edbbd17bc7c
Adam Mitha <adam.mitha@gmail.com>
2023-08-27 17:03:07 -0700

    Basic flake that imports the old system config

---
+++ configuration.nix
@@ -0,0 +1,209 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+      <home-manager/nixos>
+    ];
+
+  boot = {
+    # Bootloader.
+    loader = {
+      systemd-boot.enable = true;
+      efi.canTouchEfiVariables = true;
+    };
+  };
+
+  # Networking
+  networking = {
+    hostName = "adams-nixos-desktop";
+    networkmanager.enable = true;
+  };
+
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Set your time zone.
+  time.timeZone = "America/Vancouver";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_CA.UTF-8";
+
+  # Configure the desktop environment
+  services.xserver = {
+    # Enable the X11 windowing system.
+    enable = true;
+    # Enable the GNOME Desktop Environment.
+    desktopManager.gnome.enable = true;
+    displayManager.gdm = {
+      enable = true;
+      # Disable autosuspend
+      autoSuspend = false;
+    };
+    # Configure keymap in X11
+    layout = "us";
+    xkbVariant = "";
+    xkbOptions = "ctrl:swapcaps";
+  };
+
+  # Disable autosuspend
+  security.polkit.extraConfig = ''
+  polkit.addRule(function(action, subject) {
+      if (action.id == "org.freedesktop.login1.suspend" ||
+          action.id == "org.freedesktop.login1.suspend-multiple-sessions" ||
+          action.id == "org.freedesktop.login1.hibernate" ||
+          action.id == "org.freedesktop.login1.hibernate-multiple-sessions")
+      {
+          return polkit.Result.NO;
+      }
+  });
+  '';
+
+  console.useXkbConfig = true;
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.adam = {
+    isNormalUser = true;
+    description = "Adam Mitha";
+    extraGroups = [ "networkmanager" "wheel" "docker" ];
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAB/AenixWNi2t7mPamUlXvq7jcVH3PaLHXo6OAYpc8d adam.mitha@gmail.com"
+    ];
+    packages = with pkgs; [
+      firefox
+    #  thunderbird
+    ];
+    shell = pkgs.fish;
+  };
+  security.sudo.wheelNeedsPassword = false;
+  programs.fish = {
+    enable = true;
+  };
+
+  # Home manager
+  home-manager = {
+    useGlobalPkgs = true;
+    useUserPackages = true;
+    users.adam = import ./home.nix;
+  };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    wget
+    tmux
+    git
+    fzf
+    ripgrep
+    du-dust
+    neofetch
+    file
+    alacritty
+    google-chrome
+    lsof
+    starship
+    gh
+    bat
+    jq
+    fd
+    plocate
+    htop
+    btop
+    sysstat
+    linux-manual
+    man-pages
+    man-pages-posix
+  ];
+
+  environment.shells = with pkgs; [ fish ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # Oprograms.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+  services = {
+    openssh.enable = true;
+    tailscale.enable = true;
+    locate.enable = true;
+    sysstat.enable = true;
+  };
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  system = {
+    # This value determines the NixOS release from which the default
+    # settings for stateful data, like file locations and database versions
+    # on your system were taken. It‘s perfectly fine and recommended to leave
+    # this value at the release version of the first install of this system.
+    # Before changing this value read the documentation for this option
+    # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+    stateVersion = "23.05";
+    autoUpgrade = {
+      enable = true;
+      allowReboot = true;
+    };
+  };
+
+  nix = {
+    gc = {
+      automatic = true;
+      dates = "04:00";
+      options = "--delete-older-than 1w";
+    };
+    settings = {
+      auto-optimise-store = true;
+      experimental-features = [ "nix-command" "flakes" ];
+    };
+  };
+
+  virtualisation.docker.enable = true;
+
+  documentation = {
+    dev.enable = true;
+    man.generateCaches = true;
+  };
+}
