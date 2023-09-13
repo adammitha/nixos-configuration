23c95f3cad3d1a778ebf848da4a13ef6d251aed4
Adam Mitha <adam.mitha@gmail.com>
2023-09-12 21:32:32 -0700

    Create directory hierarchy for dotfiles

--- home.nix
+++ home.nix
@@ -16,11 +16,11 @@ in
     enable = true;
     configFile = {
       gruvbox = {
-        source = ./gruvbox.theme;
+        source = .config/fish/themes/gruvbox.theme;
 	target = "fish/themes/gruvbox.theme";
       };
       allowed_signers = {
-        source = ./allowed_signers;
+        source = .config/git/allowed_signers;
 	target = "git/allowed_signers";
       };
     };
@@ -114,44 +114,7 @@ in
       baseIndex = 1;
       enable = true;
       escapeTime = 0;
-      extraConfig = ''
-      # COLOUR (base16)
-      # default statusbar colors
-      set-option -g status-style "fg=#bdae93,bg=#3c3836"
-
-      # default window title colors
-      set-window-option -g window-status-style "fg=#bdae93,bg=default"
-
-      # active window title colors
-      set-window-option -g window-status-current-style "fg=#fabd2f,bg=default"
-
-      # pane border
-      set-option -g pane-border-style "fg=#3c3836"
-      set-option -g pane-active-border-style "fg=#504945"
-
-      # message text
-      set-option -g message-style "fg=#d5c4a1,bg=#3c3836"
-
-      # pane number display
-      set-option -g display-panes-active-colour "#b8bb26"
-      set-option -g display-panes-colour "#fabd2f"
-
-      # clock
-      set-window-option -g clock-mode-colour "#b8bb26"
-
-      # copy mode highligh
-      set-window-option -g mode-style "fg=#bdae93,bg=#504945"
-
-      # bell
-      set-window-option -g window-status-bell-style "fg=#3c3836,bg=#fb4934"
-
-      # Fix titlebar
-      set -g set-titles on
-      set -g set-titles-string "#T"
-
-      # Enable OSCYank
-      set -s set-clipboard on
-      '';
+      extraConfig = builtins.readFile .config/tmux/tmux.conf;
       historyLimit = 100000;
       mouse = true;
       terminal = "xterm-256color";
